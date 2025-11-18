7301b59fea1b1f5f8aeadd476fc9b10835cac348
Nhade <shademinecraft21@gmail.com>
2025-11-18 22:17:08 +0800

    Add project in backend

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from db import Base
+
+
+class Project(Base):
+    __tablename__ = "projects"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+
+    todos = relationship("Todo", back_populates="project", cascade="all, delete-orphan")
 
 
 class Todo(Base):
@@ -12,3 +21,6 @@ class Todo(Base):
     due = Column(DateTime)
     priority = Column(String)
     done = Column(Boolean, default=False, nullable=False)
+
+    project_id = Column(UUID(as_uuid=True), ForeignKey("projects.id"), nullable=False)
+    project = relationship("Project", back_populates="todos")
